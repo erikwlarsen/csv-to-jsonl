06f8daf36ca0394d649e914db84b876819b42c6f
erikwlarsen <erik.w.larsen@gmail.com>
2019-03-31 22:02:03 -0400

    feat: error handling and output path option for bin script

--- bin/index.js
+++ bin/index.js
@@ -7,14 +7,22 @@ const { RemoveNewLinesAndCommas } = require('../src/RemoveNewLinesAndCommas');
 const { LineSplitter } = require('../src/LineSplitter');
 const { CsvToJsonlines } = require('../src/CsvToJsonLines');
 
-const inputPath = resolve(process.cwd(), process.argv[2]);
-if (!inputPath || !/\.csv$/.test(inputPath) || !existsSync(inputPath)) {
-  throw new Error('invalid csv file path argument');
+const [,, inputPath, outputPath] = process.argv;
+if (!inputPath) {
+  throw new Error('No input file path supplied');
 }
-const outputPath = inputPath.slice(0, inputPath.length - 3).concat('jsonl');
 
-createReadStream(inputPath)
+const absInputPath = resolve(process.argv[2]);
+if (!absInputPath || !/\.csv$/.test(absInputPath) || !existsSync(absInputPath)) {
+  throw new Error('Invalid csv file path argument');
+}
+
+const absOutputPath = outputPath
+  ? resolve(outputPath)
+  : absInputPath.slice(0, absInputPath.length - 3).concat('jsonl');
+
+createReadStream(absInputPath)
   .pipe(new RemoveNewLinesAndCommas())
   .pipe(new LineSplitter())
   .pipe(new CsvToJsonlines())
-  .pipe(createWriteStream(outputPath));
+  .pipe(createWriteStream(absOutputPath));
