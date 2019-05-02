9870a736e958bbe428fbf723314fc4522cd42ce1
erikwlarsen <erik.w.larsen@gmail.com>
2019-05-01 23:32:39 -0400

    feat: add RemoveExtraQuotes transformer

--- src/RemoveNewLinesAndCommas.js
+++ src/RemoveNewLinesAndCommas.js
@@ -4,7 +4,22 @@ const {
   NEWLINE_REPLACEMENT_VALUE,
   COMMA_REPLACEMENT_VALUE_IN_QUOTES,
   NEWLINE_REPLACEMENT_VALUE_IN_QUOTES,
+  REPLACEMENT_VALUE_LENGTH,
 } = require('./constants');
+
+const augmentQuoteIndicesMapper = (matchOffset, matchLength) => index => (
+  index < matchOffset ? index : index + (REPLACEMENT_VALUE_LENGTH * matchLength)
+);
+
+const inQuotesReducer = matchOffset => (acc, val, idx) => {
+  if (typeof acc !== 'undefined') {
+    return acc;
+  }
+  if (matchOffset < val) {
+    return idx % 2 ? !this._inQuotes : this._inQuotes;
+  }
+  return undefined;
+};
 
 class RemoveNewLinesAndCommas extends Transform {
   constructor() {
@@ -14,7 +29,7 @@ class RemoveNewLinesAndCommas extends Transform {
 
   _transform(chunk, _encoding, done) {
     let string = chunk.toString();
-    const quoteIndexes = Array.from(string).reduce((arr, val, idx) => {
+    this._quoteIndices = Array.from(string).reduce((arr, val, idx) => {
       if (val === '"') {
         arr.push(idx);
       }
@@ -23,33 +38,28 @@ class RemoveNewLinesAndCommas extends Transform {
     let oldString = '';
     while (oldString !== string) {
       oldString = string;
-      string = string.replace(/\n|\r\n|\r|,/, (match, offset) => {
-        const inQuotes = quoteIndexes.reduce((acc, val, idx) => {
-          if (typeof acc !== 'undefined') {
-            return acc;
-          }
-          if (offset < val) {
-            return idx % 2 ? !this._inQuotes : this._inQuotes;
-          }
-          return undefined;
-        }, undefined);
-        if (inQuotes) {
-          return match === ','
-            ? COMMA_REPLACEMENT_VALUE_IN_QUOTES
-            : NEWLINE_REPLACEMENT_VALUE_IN_QUOTES.repeat(match.length);
-        }
-        return match === ','
-          ? COMMA_REPLACEMENT_VALUE
-          : NEWLINE_REPLACEMENT_VALUE.repeat(match.length);
-      });
+      string = string.replace(/\n|\r\n|\r|,/, this._replaceCommasAndNewlines);
     }
     this.push(string
       .replace(new RegExp(COMMA_REPLACEMENT_VALUE, 'g'), ',')
       .replace(new RegExp(NEWLINE_REPLACEMENT_VALUE, 'g'), '\n'));
-    if (quoteIndexes.length % 2) {
+    if (this._quoteIndices.length % 2) {
       this._inQuotes = !this._inQuotes;
     }
     return done();
+  }
+
+  _replaceCommasAndNewlines(match, offset) {
+    const inQuotes = this._quoteIndices.reduce(inQuotesReducer(offset), undefined);
+    this._quoteIndices = this._quoteIndices.map(augmentQuoteIndicesMapper(offset, match.length));
+    if (inQuotes) {
+      return match === ','
+        ? COMMA_REPLACEMENT_VALUE_IN_QUOTES
+        : NEWLINE_REPLACEMENT_VALUE_IN_QUOTES.repeat(match.length);
+    }
+    return match === ','
+      ? COMMA_REPLACEMENT_VALUE
+      : NEWLINE_REPLACEMENT_VALUE.repeat(match.length);
   }
 }
 
